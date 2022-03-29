7acc027392cecc4b5c135e87ec264dc43e7cd67a
Omogbai Atakpu <atakpuomogbai@gmail.com>
2022-03-29 18:07:37 +0100

    add custom sw to default sw file

--- src/service-worker.js
+++ src/service-worker.js
@@ -103,86 +103,77 @@ self.addEventListener("message", (event) => {
 //   console.log(`intercepting ${e.request.method} to ${e.request.url}`);
 // });
 
-// /* eslint-disable no-restricted-globals */
-// var STATIC_CACHE_NAME = "gfg-pwa";
-// var DYNAMIC_CACHE_NAME = "dynamic-gfg-pwa";
+/* eslint-disable no-restricted-globals */
+var STATIC_CACHE_NAME = "gfg-pwa";
+var DYNAMIC_CACHE_NAME = "dynamic-gfg-pwa";
 
-// // Add Routes and pages using React Browser Router
-// var urlsToCache = ["/", "/index.html", "../src/index.js"];
+// Add Routes and pages using React Browser Router
+var urlsToCache = ["/", "/index.html", "../src/index.js"];
 
-// // Install a service worker
-// self.addEventListener("install", (event) => {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(STATIC_CACHE_NAME).then(function (cache) {
-//       console.log("Opened cache");
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
+// Install a service worker
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(STATIC_CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-// // Cache and return requests
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((cacheRes) => {
-//       // If the file is not present in STATIC_CACHE,
-//       // it will be searched in DYNAMIC_CACHE
-//       return (
-//         cacheRes ||
-//         fetch(event.request).then((fetchRes) => {
-//           return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
-//             cache.put(event.request.url, fetchRes.clone());
-//             return fetchRes;
-//           });
-//         })
-//       );
-//     })
-//   );
-//   // if (!navigator.onLine) {
-//   //   if (
-//   //     event.request.url === `http://localhost:3000/static/js/main.chunk.js`
-//   //     // `${process.env.FRONTEND_URL}/static/js/main.chunk.js`
-//   //   ) {
-//   //     event.waitUntil(
-//   //       self.registration.showNotification("Offline", {
-//   //         body: "you are now offline",
-//   //         icon: "logo.png",
-//   //       })
-//   //     );
-//   //   }
-//   // }
-//   if (!navigator.onLine) {
-//     if (
-//       event.request.url ===
-//       `${process.env.FRONTEND_URL}/static/js/main.chunk.js`
-//     ) {
-//       event.waitUntil(
-//         self.registration.showNotification("Offline", {
-//           body: "you are now offline",
-//           icon: "logo.png",
-//         })
-//       );
-//     }
-//   }
-// });
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      // If the file is not present in STATIC_CACHE,
+      // it will be searched in DYNAMIC_CACHE
+      return (
+        cacheRes ||
+        fetch(event.request).then((fetchRes) => {
+          return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
+  if (!navigator.onLine) {
+    if (
+      event.request.url ===
+      `${process.env.FRONTEND_URL}/static/js/main.chunk.js`
+    ) {
+      event.waitUntil(
+        self.registration.showNotification("Offline", {
+          body: "you are now offline",
+          icon: "logo.png",
+        })
+      );
+    }
+  }
+  // if (!navigator.onLine) {
+  //   event.waitUntil(
+  //     self.registration.showNotification("Offline", {
+  //       body: "you are now offline",
+  //       icon: "logo.png",
+  //     })
+  //   );
+  //   }
+});
 
-// // Update a service worker
-// self.addEventListener("activate", (event) => {
-//   var cacheWhitelist = ["gfg-pwa"];
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         // eslint-disable-next-line array-callback-return
-//         cacheNames.map((cacheName) => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
-
-// // window.addEventListener("offline", function () {
-// //   console.log("Oh no, you lost your network connection.");
-// // });
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["gfg-pwa"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        // eslint-disable-next-line array-callback-return
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
