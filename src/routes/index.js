07d54dd4b8c97625ccf5430de5513389e460c3e7
Omogbai Atakpu <atakpuomogbai@gmail.com>
2022-03-30 08:43:59 +0100

    remove the icons logic from index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Discover from "./Discover";
-import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
-
-initializeIcons();
 
 export default function Routes() {
   // Here you'd return an array of routes
